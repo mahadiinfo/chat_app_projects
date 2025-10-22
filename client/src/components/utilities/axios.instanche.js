1f7cb5ee47a49f9301c25da51525414488c26a06
Mahadi Hasan <mahadiinfo88@gmail.com>
2025-10-22 14:03:54 +0600

    Update DB_URL to include HTTPS protocol

--- client/src/components/utilities/axios.instanche.js
+++ client/src/components/utilities/axios.instanche.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const DB_URL = "chatappprojects-production.up.railway.app/api/v1";
+const DB_URL = "https://chatappprojects-production.up.railway.app";
 export const axiosIns = axios.create({
   baseURL: DB_URL,
   withCredentials: true,
