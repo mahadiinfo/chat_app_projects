aba3765b601e92e16ade83100a7a8c5ff2471f54
Mahadi Hasan <mahadiinfo88@gmail.com>
2025-10-22 11:16:32 +0600

    Use environment variable for MongoDB connection URL

--- server/db/connection.db.js
+++ server/db/connection.db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDb = async ()=>{
     try{
 
-       const mongodb_url = "mongodb+srv://mahadiinfo888_db_user:xvwLD7Jk5BUgg9n6@chatappdb.gml5rak.mongodb.net/";
+       const mongodb_url = process.env.MONGODB_API;
         const responsedb = await mongoose.connect(mongodb_url)
         console.log(`database connected: ${responsedb.connection.host}`)
      } catch(error1) {
